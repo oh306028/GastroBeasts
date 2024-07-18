5fc0a4ee991533fcb0459a86ac26d53ef825f337
Oskar <hamerlao@gmail.com>
2024-07-18 14:39:26 +0200

    Added the configuration with connection string to the db

--- App/Program.cs
+++ App/Program.cs
@@ -1,4 +1,6 @@
 
+using Microsoft.EntityFrameworkCore;
+
 namespace App
 {
     public class Program
@@ -13,6 +15,13 @@ namespace App
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
 
             var app = builder.Build();
 
