a0cc21fa406e7dfe8003ed360dc6303977084890
Oskar <hamerlao@gmail.com>
2024-07-18 16:31:02 +0200

    Added restaurantCategory join table

---
+++ App/Entities/RestaurantCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Entities
+{
+    public class RestaurantCategory
+    {
+        public int RestaurantId { get; set; }
+        public virtual  Restaurant Restaurant { get; set; }
+
+
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+
+
+    }
+}
