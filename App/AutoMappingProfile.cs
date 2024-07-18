a295fd0dc879c0affb0ee517ac49a1083d2d4f0b
Oskar <hamerlao@gmail.com>
2024-07-18 23:23:33 +0200

    added the automapper profile

---
+++ App/AutoMappingProfile.cs
@@ -0,0 +1,40 @@
+﻿using App.Dtos.CreateDtos;
+using App.Dtos.DisplayDtos;
+using App.Entities;
+using AutoMapper;
+
+namespace App
+{
+    public class AutoMappingProfile : Profile   
+    {
+        public AutoMappingProfile() 
+        {
+            CreateMap<CreateRestaurantDto, Restaurant>();
+
+            CreateMap<Address, AddressDto>();
+
+            CreateMap<User, UserDto>(); 
+
+            CreateMap<Stars, StarsDto>();
+
+            CreateMap<Category, CategoryDto>()
+                .ForMember(dest => dest.TopDishes, opt => opt.MapFrom(src => src.TopDish));
+
+            CreateMap<TopDish, TopDishDto>();
+
+            CreateMap<Restaurant, RestaurantDto>()
+                 .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Address))
+                   .ForMember(dest => dest.Categories, opt => opt.MapFrom(src => src.RestaurantCategories.Select(rc => rc.Category)))
+                   .ForMember(dest => dest.Reviews, opt => opt.MapFrom(src => src.Reviews));
+
+
+            CreateMap<Review, ReviewDto>()
+                .ForMember(dest => dest.Stars, opt => opt.MapFrom(src => src.Stars))
+                .ForMember(dest => dest.ReviewedBy, opt => opt.MapFrom(src => src.ReviewedBy));
+
+       
+
+
+        }
+    }
+}
