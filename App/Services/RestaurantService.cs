20e81ff521d6339da58c7cb125dc92e3e62b25a2
Oskar <hamerlao@gmail.com>
2024-07-18 22:52:05 +0200

    created restaurantService

---
+++ App/Services/RestaurantService.cs
@@ -0,0 +1,59 @@
+﻿using App.Dtos;
+using App.Entities;
+using App.Exceptions;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Services
+{
+    public interface IRestaurantService
+    {
+        IEnumerable<RestaurantDto> GetAllRestaurants();
+        RestaurantDto GetRestaurantById(int id);
+    }   
+
+    public class RestaurantService : IRestaurantService
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public RestaurantService(AppDbContext dbContext, IMapper mapper)    
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+
+        public IEnumerable<RestaurantDto> GetAllRestaurants()
+        {
+            var restaurants = _dbContext.Restaurants
+                .Include(a => a.Address)
+                .Include(rc => rc.RestaurantCategories)
+                .ThenInclude(c => c.Category)
+                .Include(rv => rv.Reviews);
+
+            return _mapper.Map<List<RestaurantDto>>(restaurants);   
+        }
+
+
+        public RestaurantDto GetRestaurantById(int id)
+        {
+            var restaurant = _dbContext.Restaurants.Include(a => a.Address)
+                .Include(rc => rc.RestaurantCategories)
+                .ThenInclude(c => c.Category)
+                .Include(rv => rv.Reviews)
+                .FirstOrDefault(i => i.Id == id);
+
+
+
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+
+            var restaurantDto = _mapper.Map<RestaurantDto>(restaurant);
+
+            return restaurantDto;
+
+        }
+    }
+}
