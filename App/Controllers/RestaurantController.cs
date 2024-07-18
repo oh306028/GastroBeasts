9e5c0d6c15626e43b2e51fdfec1c638a2cd15315
Oskar <hamerlao@gmail.com>
2024-07-18 22:51:47 +0200

    created the restaurantController

---
+++ App/Controllers/RestaurantController.cs
@@ -0,0 +1,40 @@
+﻿using App.Dtos;
+using App.Entities;
+using App.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    [Route("api/restaurants")]
+    [ApiController]
+    public class RestaurantController : ControllerBase  
+    {
+        private readonly IRestaurantService _restaurantService;
+
+        public RestaurantController(IRestaurantService restaurantService)
+        {
+            _restaurantService = restaurantService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantDto>> GetRestaurants()
+        {
+            var restaurants = _restaurantService.GetAllRestaurants();
+
+            return Ok(restaurants);
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<RestaurantDto> GetRestaurantById([FromRoute]int id)
+        {
+            var restaurant = _restaurantService.GetRestaurantById(id);
+
+            return Ok(restaurant);
+        }
+
+
+
+
+    }
+}
