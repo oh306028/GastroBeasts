bcac4c7cbd1b120ffc39ace027d0aeaa80e5eb6c
Oskar <hamerlao@gmail.com>
2024-07-19 13:11:40 +0200

    Create address and review createDtos

---
+++ App/Dtos/CreateDtos/CreateAddressDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Dtos.CreateDtos
+{
+    public class CreateAddressDto
+    {
+        [Required, MaxLength(20)]
+        public string City { get; set; }
+
+        [Required, MaxLength(25)]
+        public string Street { get; set; }
+
+        [Required, MaxLength(10)]
+        public string Number { get; set; }      
+    }
+}
---
+++ App/Dtos/CreateDtos/CreateReviewDto.cs
@@ -0,0 +1,16 @@
+﻿using App.Dtos.DisplayDtos;
+using System.ComponentModel.DataAnnotations;
+
+namespace App.Dtos.CreateDtos
+{
+    public class CreateReviewDto
+    {
+        [MaxLength(100)]
+        public string Comment { get; set; }
+
+        [Required, Range(1,5)]
+        public int Stars { get; set; }
+
+        public int RestaurantId { get; set; }   
+    }
+}
