fc872214823f4668e03d837c22b3234f110c8f71
Oskar <hamerlao@gmail.com>
2024-07-19 13:03:11 +0200

    added address and review controllers

---
+++ App/Controllers/AddressController.cs
@@ -0,0 +1,42 @@
+﻿using App.Dtos.CreateDtos;
+using App.Dtos.DisplayDtos;
+using App.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    [Route("api/restaurants/{restaurantId}/address")]
+    [ApiController]
+   
+    public class AddressController : ControllerBase
+    {
+        private readonly IAddressService _addressService;
+
+        public AddressController(IAddressService addressService)
+        {
+            _addressService = addressService;
+        }
+
+
+
+        [HttpGet]
+        public ActionResult<AddressDto> GetAddress([FromRoute] int restaurantId)
+        {
+            var address = _addressService.GetAddressFromRestaurant(restaurantId);
+
+            return Ok(address);
+
+        }
+
+
+        [HttpPost]
+        public ActionResult<int> CreateAddress([FromRoute] int restaurantId, [FromBody] CreateAddressDto dto)
+        {
+            int addressId = _addressService.CreateAddressToRestaurant(restaurantId, dto);
+
+            return Created($"api/restaurants/{restaurantId}/address/{addressId}", null);
+        }
+
+    }
+}
---
+++ App/Controllers/ReviewController.cs
@@ -0,0 +1,32 @@
+﻿using App.Dtos.CreateDtos;
+using App.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    [Route("api/restaurants/{restaurantId}/reviews")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewService _reviewService;
+
+        public ReviewController(IReviewService reviewService)
+        {
+            _reviewService = reviewService;
+        }
+
+
+        [HttpPost]
+        public ActionResult<int> CreateReview([FromBody] CreateReviewDto dto, [FromRoute] int restaurantId)
+        {
+            int createdReviewId = _reviewService.CreateReviewToRestaurant(dto, restaurantId);
+
+            return Created($"api/restaurants/{restaurantId}/reviews/{createdReviewId}", null);
+
+
+        }
+
+
+    }
+}
