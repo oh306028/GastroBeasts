40d570b2bae16ee9c83b364ec953cac8b1c47270
Oskar <hamerlao@gmail.com>
2024-07-19 13:02:16 +0200

    Created the addressService and reviewService

---
+++ App/Services/AddressService.cs
@@ -0,0 +1,68 @@
+﻿using App.Dtos.CreateDtos;
+using App.Dtos.DisplayDtos;
+using App.Entities;
+using App.Exceptions;
+using AutoMapper;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+namespace App.Services
+{
+    public interface IAddressService
+    {
+        AddressDto GetAddressFromRestaurant([FromRoute] int restaurantId);
+        int CreateAddressToRestaurant(int restaurantId, CreateAddressDto dto);
+    }
+
+    public class AddressService : IAddressService   
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        private Restaurant CheckRestaurant(int restaurantId)
+        {
+            var restaurant = _dbContext.Restaurants.Include(a => a.Address).FirstOrDefault(i => i.Id == restaurantId);
+
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            return restaurant;
+        }
+
+        public AddressService(AppDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public AddressDto GetAddressFromRestaurant(int restaurantId)
+        {
+            
+            var restaurant = CheckRestaurant(restaurantId);
+
+            var address = restaurant.Address;
+            var mappedAddress = _mapper.Map<AddressDto>(address);
+
+          
+            return (mappedAddress);
+
+
+        }
+
+        public int CreateAddressToRestaurant(int restaurantId, CreateAddressDto dto)
+        {
+            var restaurant = CheckRestaurant(restaurantId);
+
+            var createdAddress = _mapper.Map<Address>(dto);
+            createdAddress.Restaurant = restaurant;
+
+            _dbContext.Addresses.Add(createdAddress);
+            _dbContext.SaveChanges();
+
+            return createdAddress.Id;
+
+        }
+    }
+}
---
+++ App/Services/ReviewService.cs
@@ -0,0 +1,57 @@
+﻿using App.Dtos.CreateDtos;
+using App.Entities;
+using App.Exceptions;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Services
+{
+    public interface IReviewService
+    {
+        int CreateReviewToRestaurant(CreateReviewDto dto, int restaurantId);
+    }
+
+    public class ReviewService : IReviewService
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public ReviewService(AppDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;    
+        }
+
+        private Restaurant CheckRestaurant(int restaurantId)
+        {
+            var restaurant = _dbContext.Restaurants.FirstOrDefault(i => i.Id == restaurantId);
+
+            if (restaurant is null)
+                throw new NotFoundException("Restaurant not found");
+
+            return restaurant;
+        }
+
+        public int CreateReviewToRestaurant(CreateReviewDto dto, int restaurantId)
+        {
+            var restaurant = CheckRestaurant(restaurantId);
+
+  
+            var createdReview = new Review()
+            {
+                Comment = dto.Comment,
+                StarsId = dto.Stars,
+                Restaurant = restaurant,
+                UserId = 1
+                
+            };
+
+            
+            _dbContext.Reviews.Add(createdReview);
+            _dbContext.SaveChanges();
+
+            return createdReview.Id;
+
+        }
+    }
+}
