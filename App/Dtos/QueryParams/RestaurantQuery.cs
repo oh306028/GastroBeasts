9a6901226310e81c1226951065bb3acf4e672e4d
Oskar <hamerlao@gmail.com>
2024-07-19 20:40:22 +0200

    created the queryParams class

---
+++ App/Dtos/QueryParams/RestaurantQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace App.Dtos.QueryParams
+{
+    public class RestaurantQuery
+    {
+        public bool? IncludeReviews { get; set; }
+        public string? CategoryName { get; set; }
+        public string? RestaurantName { get; set; }  
+
+    }
+}
