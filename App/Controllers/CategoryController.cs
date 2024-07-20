ec021689682878da69e7dc742ee1a703b424ed26
Oskar <hamerlao@gmail.com>
2024-07-20 12:10:12 +0200

    created the category controller and service for displaying current restaurant's categories - in future creating more categories

---
+++ App/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using App.Dtos.DisplayDtos;
+using App.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+
+    [Route("api/restaurants/categories")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+           _categoryService = categoryService;
+        }
+
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<RestaurantCategoriesDto>> GetRestaurantCategories()
+        {
+            var categories = _categoryService.GetAllRestaurantCategories();
+
+            return Ok(categories);
+        }
+
+
+    }
+}
---
+++ App/Services/CategoryService.cs
@@ -0,0 +1,36 @@
+﻿using App.Dtos.DisplayDtos;
+using AutoMapper;
+
+namespace App.Services
+{
+    public interface ICategoryService
+    {
+        IEnumerable<RestaurantCategoriesDto> GetAllRestaurantCategories();
+    }
+        
+    public class CategoryService : ICategoryService
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+
+        public CategoryService(AppDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+
+        public IEnumerable<RestaurantCategoriesDto> GetAllRestaurantCategories()
+        {
+            var categories = _dbContext.Categories.ToList();
+
+            var mappedCategories = _mapper.Map<List<RestaurantCategoriesDto>>(categories);
+
+            return mappedCategories;
+        }
+
+
+
+    }
+}
