e5077e76bcbc868718f4a1f9eaffa1b5378bef65
Oskar <hamerlao@gmail.com>
2024-07-22 13:35:02 +0200

    Added new createNewCategory dto

---
+++ App/Dtos/CreateDtos/CreateNewCategoryDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Dtos.CreateDtos
+{
+    public class CreateNewCategoryDto
+    {
+        [Required, MaxLength(25)]
+        public string Name { get; set; }    
+    }
+}
