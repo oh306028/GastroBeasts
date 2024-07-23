5c5a29625e48ca13cdec99a2640ef0c14ffb23a4
Oskar Hamerla <hamerlao@gmail.com>
2024-07-23 22:38:10 +0200

    Preapared the controller and service for user managment

---
+++ App/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using App.Dtos.CreateDtos;
+using App.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+       
+        [HttpPost("register")]
+        public ActionResult RegisterUser(RegisterUserDto dto)
+        {
+            _userService.RegisterNewUser(dto);
+
+            return Created("", null);
+        }
+
+
+
+
+
+
+    }
+}
