40afd6a2a2010c5729dee034a365e9d2d0b5a224
Oskar <hamerlao@gmail.com>
2024-07-24 23:40:07 +0200

    created the userContextService to store the currentUser information. UserId and User reference

---
+++ App/Services/UserContextService.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+
+namespace App.Services
+{
+    public interface IUserContextService
+    {
+        ClaimsPrincipal User { get; }
+        int? UserId { get; }    
+    }
+
+    public class UserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public UserContextService(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+        public ClaimsPrincipal User => _contextAccessor.HttpContext?.User;
+        public int? UserId => User is null ? null : (int?)int.Parse(User.FindFirst(p => p.Type == ClaimTypes.NameIdentifier).Value);
+
+    }
+}
