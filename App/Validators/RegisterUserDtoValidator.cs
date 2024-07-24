000b76e2143a31c85d72997ddb0e7ed64510846f
Oskar <hamerlao@gmail.com>
2024-07-24 22:27:36 +0200

    created registerUserDto validator; validating if email and nickname is used, confirm password equals password

---
+++ App/Validators/RegisterUserDtoValidator.cs
@@ -0,0 +1,53 @@
+﻿using App.Dtos.CreateDtos;
+using FluentValidation;
+
+namespace App.Validators
+{
+    public class RegisterUserDtoValidator : AbstractValidator<RegisterUserDto>  
+    {
+        public RegisterUserDtoValidator(AppDbContext dbContext)
+        {
+          
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(cp => cp.Password)
+                .NotNull(); 
+
+            RuleFor(x => x.NickName)
+                .NotNull()
+                .MaximumLength(20);
+
+            RuleFor(x => x.Email)
+                .NotNull()
+                .EmailAddress();
+
+            RuleFor(p => p.Password)
+                .NotNull();
+
+            RuleFor(e => e.Email)
+                .Custom((value, context) => {
+                    var emailInUse = dbContext.Users.Any(e => e.Email == value);   
+                        
+                    if(emailInUse)
+                    {
+                        context.AddFailure("Email", "Email is already taken");
+                    }
+
+            });
+
+
+            RuleFor(n => n.NickName)
+                .Custom((value, context) =>
+                {
+
+                    var nickInUse = dbContext.Users.Any(n => n.NickName == value);
+
+                    if (nickInUse)
+                    {
+                        context.AddFailure("NickName", "Name is already taken");
+                    }
+
+                });
+           
+        }
+    }
+}
