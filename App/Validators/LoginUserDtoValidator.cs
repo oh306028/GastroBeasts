5572cecec5869f3fb12d49afa1d1ca04418b7da8
Oskar <hamerlao@gmail.com>
2024-07-25 10:46:47 +0200

    UserLogin dto validator added; password and email validates now

---
+++ App/Validators/LoginUserDtoValidator.cs
@@ -0,0 +1,19 @@
+﻿using App.Dtos.CreateDtos;
+using FluentValidation;
+
+namespace App.Validators
+{
+    public class LoginUserDtoValidator : AbstractValidator<LoginUserDto>
+    {
+        public LoginUserDtoValidator()
+        {
+            RuleFor(e => e.Email)   
+                .NotNull()
+                .EmailAddress();
+
+            RuleFor(p => p.Password)
+                .NotNull()
+                .MinimumLength(5);
+        }
+    }
+}
