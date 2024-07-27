291f3ec49c3c2923f86985171922a171080f87f7
Oskar <hamerlao@gmail.com>
2024-07-27 08:31:42 +0200

    Added the getById request; firsly creating a model seeding db and then deletin it manually; also bad request testing for invalid model added

--- IntegrationTests/RestaurantControllerTests.cs
+++ IntegrationTests/RestaurantControllerTests.cs
@@ -1,5 +1,6 @@
 ﻿using App;
 using App.Dtos.CreateDtos;
+using App.Entities;
 using FluentAssertions;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -15,11 +16,12 @@ namespace IntegrationTests
     public class RestaurantControllerTests : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly HttpClient _client;
+        private WebApplicationFactory<Program> _factory;    
 
         public RestaurantControllerTests(WebApplicationFactory<Program> factory)
         {
 
-            _client = factory.WithWebHostBuilder(builder =>
+            _factory = factory.WithWebHostBuilder(builder =>
             {
                 builder.ConfigureTestServices(services =>
                 {
@@ -41,12 +43,48 @@ namespace IntegrationTests
                      options.UseInMemoryDatabase("TestDb"));
 
                 });
-            })
-       .CreateClient();
+            });
+
+            _client = _factory.CreateClient();
+      
 
             _client.DefaultRequestHeaders.Authorization =
        new AuthenticationHeaderValue(scheme: "TestScheme");
 
+
+        }
+
+
+        [Fact]
+        public async Task GetRestaurantById_WithValidIdAndModel_ReturnsOk()
+        {
+
+
+            var restaurant = new Restaurant()
+            {
+                Name = "test rest",
+                Description = "test desc",
+            };
+
+
+            var serviceScope = _factory.Services.GetService<IServiceScopeFactory>();
+
+            using var scope = serviceScope.CreateScope();
+
+            var _dbContextScope = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+            _dbContextScope.Restaurants.Add(restaurant);
+            _dbContextScope.SaveChanges();
+
+            var restaurantToGet = _dbContextScope.Restaurants.First(n => n.Name == "test rest");
+
+            var response = await _client.GetAsync($"/api/restaurants/{restaurantToGet.Id}");    
+                
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        
+            _dbContextScope.Restaurants.Remove(restaurant);
+            _dbContextScope.SaveChanges();
 
         }
 
@@ -67,6 +105,23 @@ namespace IntegrationTests
 
         }
 
+
+        [Theory]
+        [InlineData("oneNamethat is already too long to be a restaurant name so its shouldnt be created right now")]
+        public async Task CreateRestaurant_WithInvalidModel_ReturnsBadRequest(string name)
+        {
+            var restaurantModel = new CreateRestaurantDto()
+            {
+                Name = name
+            };
+
+
+            var response = await _client.PostAsJsonAsync("/api/restaurants",restaurantModel);
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+
+
+
+        }
 
 
 
