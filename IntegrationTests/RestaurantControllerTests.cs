c7c03697335a606900257aa8b9580e073c8cf4d7
Oskar <hamerlao@gmail.com>
2024-07-27 07:48:32 +0200

    Created integration tests; simple get request; also post request that have to be authorized; use fake authenticationHandler

---
+++ IntegrationTests/RestaurantControllerTests.cs
@@ -0,0 +1,104 @@
+﻿using App;
+using App.Dtos.CreateDtos;
+using FluentAssertions;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Data.Common;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace IntegrationTests
+{
+    public class RestaurantControllerTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public RestaurantControllerTests(WebApplicationFactory<Program> factory)
+        {
+
+            _client = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddAuthentication(o =>
+                    {
+                        o.DefaultAuthenticateScheme = "TestScheme";
+                        o.DefaultChallengeScheme = "TestScheme";
+                    }).AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>(
+                            "TestScheme", options => { });
+                });
+            })
+       .CreateClient();
+
+            _client.DefaultRequestHeaders.Authorization =
+       new AuthenticationHeaderValue(scheme: "TestScheme");
+
+
+        }
+
+
+        [Fact]
+        public async Task CreateRestaurant_WithValidModel_ReturnsOk()
+        {
+            var restaurantModel = new CreateRestaurantDto()
+            {
+                Name = "Test Rest",
+                Description = "Test descr"
+            };
+
+            
+               
+            var response = await _client.PostAsJsonAsync("/api/restaurants", restaurantModel);
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
+
+        }
+
+
+
+
+        [Fact]
+        public async Task GetRestaurants_ValidQuery_ReturnStatusCodeOk()
+        {
+          
+            var response = await _client.GetAsync("/api/restaurants");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK); 
+
+        }
+
+
+        [Theory]
+        [InlineData("includeReviews=true")]
+        [InlineData("includeReviews=true&restaurantName=keb")]
+        public async Task GetRestaurants_ValidQueryWithParams_ReturnStatusCodeOk(string phrase)
+        {   
+
+            var response = await _client.GetAsync($"/api/restaurants?{phrase}");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+
+        }
+
+
+        [Theory]
+        [InlineData("all")]
+        [InlineData("include=false")]
+        [InlineData("name=keb")]
+
+        public async Task GetRestaurants_WithInvalidQuery_ReturnsBadRequest(string phrase)
+        {
+            var response = await _client.GetAsync($"/api/restaurants/{phrase}");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+
+
+
+
+    }
+}
---
+++ IntegrationTests/TestAuthenticationHandler.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+
+namespace IntegrationTests
+{
+    public class TestAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public TestAuthenticationHandler(IOptionsMonitor<AuthenticationSchemeOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var claims = new[] { new Claim(ClaimTypes.Name, "Test user") };
+            var identity = new ClaimsIdentity(claims, "TestScheme");
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, "TestScheme");
+
+            var result = AuthenticateResult.Success(ticket);
+
+            return Task.FromResult(result);
+        }
+    }
+}
