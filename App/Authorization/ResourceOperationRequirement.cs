e0725302f306c654b62dafe0067c5a6bd1430892
Oskar <hamerlao@gmail.com>
2024-07-27 10:49:58 +0200

    Created the operationResource requirement; requirementHandler

---
+++ App/Authorization/ResourceOperationRequirement.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace App.Authorization
+{
+
+    public enum OperationType
+    {
+        Create,
+        Update,
+        Delete,
+        Read              
+    }
+
+    public class ResourceOperationRequirement : IAuthorizationRequirement
+    {
+        public OperationType OperationType { get; set; }
+
+        public ResourceOperationRequirement(OperationType operationType)
+        {
+            OperationType = operationType;
+        }
+    }
+
+
+}
---
+++ App/Authorization/ResourceOperationRequirementHandler.cs
@@ -0,0 +1,34 @@
+﻿using App.Entities;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace App.Authorization
+{
+    public class ResourceOperationRequirementHandler : AuthorizationHandler<ResourceOperationRequirement, Review>
+    {
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ResourceOperationRequirement requirement, Review review)        
+        {
+
+            if(requirement.OperationType == OperationType.Read || requirement.OperationType == OperationType.Create)
+            {
+                context.Succeed(requirement);
+            }
+
+
+            if (requirement.OperationType == OperationType.Delete || requirement.OperationType == OperationType.Update)
+            {
+                var userId = int.Parse(context.User.FindFirst(c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+                if(userId == review.ReviewedBy.Id)
+                {
+                    context.Succeed(requirement);
+                }
+
+            }
+
+            return Task.CompletedTask;
+
+
+        }
+    }
+}
