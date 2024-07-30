31eec689eaa2b1e4e2f583e11589fc70b84f00b5
Oskar <hamerlao@gmail.com>
2024-07-30 10:26:22 +0200

    Created validator for restaurantQuery; not null page size and page number

---
+++ App/Validators/RestaurantQueryParamsValidator.cs
@@ -0,0 +1,16 @@
+﻿using App.Dtos.CreateDtos;
+using App.Dtos.QueryParams;
+using FluentValidation;
+
+namespace App.Validators
+{
+    public class RestaurantQueryParamsValidator : AbstractValidator<RestaurantQuery>
+    {
+        public RestaurantQueryParamsValidator()
+        {
+            RuleFor(ps => ps.PageSize).NotNull().GreaterThanOrEqualTo(1);
+
+            RuleFor(pn => pn.PageNumber).NotNull().GreaterThanOrEqualTo(1);
+        }
+    }
+}
