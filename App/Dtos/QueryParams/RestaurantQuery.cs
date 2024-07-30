12b255400fa9b4017484f6676ce044911bdc5de7
Oskar <hamerlao@gmail.com>
2024-07-30 10:17:52 +0200

    Refactored the restaurantQuery; added the pagination fields

--- App/Dtos/QueryParams/RestaurantQuery.cs
+++ App/Dtos/QueryParams/RestaurantQuery.cs
@@ -1,10 +1,15 @@
-﻿namespace App.Dtos.QueryParams
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace App.Dtos.QueryParams
 {
     public class RestaurantQuery
     {
         public bool? IncludeReviews { get; set; }
         public string? CategoryName { get; set; }
-        public string? RestaurantName { get; set; }  
+        public string? RestaurantName { get; set; }
+
+        public int PageSize { get; set; }
+        public int PageNumber { get; set; }
 
     }
 }
