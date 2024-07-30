af8b77dd1aba7823b3e3d89ff089919a615a5b24
Oskar <hamerlao@gmail.com>
2024-07-30 10:15:37 +0200

    Added the PagedResults dto; for paginated restaurants; made the class generic for future results

---
+++ App/Dtos/Results/PagedResults.cs
@@ -0,0 +1,34 @@
+﻿using App.Dtos.DisplayDtos;
+using App.Entities;
+
+namespace App.Dtos.Results
+{
+    public class PagedResults<T>    
+    {
+        public IEnumerable<T> Items { get; set; }   
+
+        public int ResultFrom { get; set; }
+        public int ResultTo { get; set; }
+
+
+        public int Pages { get; set; }
+        public int AllResultsCount { get; set; }    
+
+        public PagedResults(IEnumerable<T> items, int pageSize, int pageNumber, int resultsCount)   
+        {
+            Items = items;
+            ResultFrom = ((pageNumber - 1) * pageSize) + 1;
+            ResultTo = pageSize * pageNumber;        
+            Pages = (int)Math.Ceiling((double)resultsCount / pageSize);
+            AllResultsCount = resultsCount;
+
+        }
+
+
+
+    }
+
+
+
+    
+}
